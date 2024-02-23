1b98bc539824aa4581d96cf1c4e5a491a9811a88
Alexandre <60877715+reisemh3@users.noreply.github.com>
2024-02-23 15:11:58 +0100

    Mise en place de l interface React ts, tsx et des différents dossiers

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
+import './index.css';
+import './styles/layout.css';
+import './styles/pages.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './layout/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {HashRouter} from "react-router-dom";
+// import {migrateSavedSettings_V1} from "./utils/LocalStorageMigration";
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
