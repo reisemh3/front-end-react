fd5c13a11f9500f495b08263618a62d3f5cfd80d
Abder100 <ayourmeche@gmail.com>
2024-02-26 16:51:42 +0100

    V2 page Livres
    "liste de livres tempo"

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+
 import './BookCard.css';
 export default BookCard;
 
 function BookCard({ title, author }) {
 
     return (
-        <div className='card'>
+        <div className='card w-80'>
             <div className='info'>
                 <h3 className='title'>{title}</h3>
                 <p className='author'>Auteur : {author}</p>
--- src/pages/livres/Livres.tsx
+++ src/pages/livres/Livres.tsx
@@ -1,53 +1,63 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, {Dispatch, SetStateAction, useContext, useEffect} from 'react';
+import React, {Dispatch, SetStateAction,useState, useContext, useEffect} from 'react';
 import './Livres.css';
 import './../../styles/pages.css';
-import SearchBar from '../../components/SearchBar';
 import BookCard from '../../components/BookCard';
 
 // import Header from "../../components/Header";
 
 function Livres(){
-const handleSearch = () => {
-  // rajouté le code pour faire les appelles à l'API 
-  console.log('Search query');
-};
 
+  const [searchType, setSearchType] = useState<'id' | 'author' | 'text'>('id');
+  const [searchValue, setSearchValue] = useState('');
 
+  const handleSearch = () => {
+    if (searchType === 'id') {
+      // Recherche par id
+    } else if (searchType === 'author') {
+      // Recherche par auteur
+    } else {
+      // Recherche par texte
+    }
+  };
+
+/*<div className='header'>
+              <h1 className='title'>Mon Moteur de Recherche</h1>
+              <SearchBar onSearch={handleSearch} />
+          </div>
+*/
   return (
         <div className='container'>
-        <div className='header'>
-            <h1 className='title'>Mon Moteur de Recherche</h1>
-            <SearchBar onSearch={handleSearch} />
+          
+          <h1 className="bg-blue-500 text-white rounded">Recherche sur les Livres</h1>
+          
+          <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="border p-2 rounded flex-grow min-w-min max-w-screen-md" />
+          <button onClick={handleSearch} className="text-white p-2 rounded">Rechercher</button>
+          
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <div className='grid grid-cols-none'>
+              <BookCard
+                  title="Titre du Livre 1"
+                  author="Auteur 1"
+                  /*coverImage="https://example.com/book1-cover.jpg"*/
+              />
+
+              <BookCard
+                  title="Titre du Livre 2"
+                  author="Auteur 2"
+                  /*coverImage="https://example.com/book2-cover.jpg"*/
+              />  
+              <BookCard
+                  title="Titre du Livre 3"
+                  author="Auteur 3"
+                  /*coverImage="https://example.com/book3-cover.jpg"*/
+              />  
+          </div>
         </div>
-
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-
-
-        <div className='booklist'>
-            <BookCard
-                title="Titre du Livre 1"
-                author="Auteur 1"
-                /*coverImage="https://example.com/book1-cover.jpg"*/
-            />
-
-            <BookCard
-                title="Titre du Livre 2"
-                author="Auteur 2"
-                /*coverImage="https://example.com/book2-cover.jpg"*/
-            />  
-            <BookCard
-                title="Titre du Livre 3"
-                author="Auteur 3"
-                /*coverImage="https://example.com/book3-cover.jpg"*/
-            />  
-        </div>
-    </div>
   );
 }
 export default Livres;
+
